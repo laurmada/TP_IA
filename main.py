077cf4bfa4bcf02e1beadc70aa19d132680682b5
Laurmada <galaxiaedits@gmail.com>
2026-06-25 10:45:43 -0300

    debug main

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ def main():
     ginasios = carregar_ginasios()
 
     # debug 
+    """
     for ginasio in ginasios:
         print(ginasio.nome, ginasio.tipo)
         for pokemon in ginasio.pokemons:
             print(" ", pokemon.nome, pokemon.tipos)
-    
+    """
+    """
+    for pokemon in pokemons:
+    print(pokemon.nome, pokemon.tipos)
+    for atributo, valor in pokemon.atributos.items():
+        print(f"  {atributo}: {valor}")
+    """
+
 if __name__ == "__main__":
     main()
