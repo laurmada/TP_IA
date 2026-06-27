7efdc78adbc4f4743eaf1b14a2181ef661d4abe8
laurmada <lauramadaleno001@gmail.com>
2026-06-27 00:02:42 -0300

    define logica de "batalha"

--- funcoes.py
+++ funcoes.py
@@ -25,23 +25,6 @@ vantagens_forte_contra = {
     "fada": ["lutador", "dragao", "noturno"]
 }
 
-# verifica vantagem de tipo de pokemon pra pokemn
-def vantagem(pokemon_time, pokemon_ginasio):
-    if pokemon_ginasio in vantagens_forte_contra.get(pokemon_time, []):
-        return 2 
-    elif pokemon_time in vantagens_forte_contra.get(pokemon_ginasio, []):
-        return 0.5
-    else: 
-        return 1
-
-# calcula a vantage de tipo geral do time contra o time do ginasio   
-def calcular_vantagem(pokemon_time, pokemon_ginasio):
-    valor_total = 0
-    for tipo_atk in pokemon_time.tipos:
-        for tipo_def in pokemon_ginasio.tipos:
-            valor_total += vantagem(tipo_atk, tipo_def)
-    return valor_total
-
 # carrega o json dos pokemons e armazena em uma lista
 def carregar_pokemons():
     caminho = os.path.join(os.path.dirname(__file__), "dados", "pokemons.json")
@@ -98,4 +81,47 @@ def selecionar_ginasios(ginasios, quantidade):
         return random.sample(ginasios, random.randint(1, len(ginasios)))
     return random.sample(ginasios, min(quantidade, len(ginasios)))
 
+# verifica vantagem de tipo de pokemon pra pokemon e penaliza ou da beneficios
+def vantagem(pokemon_time, pokemon_ginasio):
+    if pokemon_ginasio in vantagens_forte_contra.get(pokemon_time, []):
+        return 2 
+    elif pokemon_time in vantagens_forte_contra.get(pokemon_ginasio, []):
+        return 0.5
+    else: 
+        return 1
 
+# calcula a vantage de tipo geral do time contra o time do ginasio   
+def calcular_vantagem(pokemon_time, pokemon_ginasio):
+    valor_total = 0
+    for tipo_atk in pokemon_time.tipos:
+        for tipo_def in pokemon_ginasio.tipos:
+            valor_total *= vantagem(tipo_atk, tipo_def)
+    return valor_total
+
+# define o poder total do pokemon
+def poder_pokemon(pokemon):
+    atributos = pokemon.atributos
+    return atributos["ataque"] + atributos["ataque_especial"] + atributos["velocidade"]
+
+# logica para o ataque e defesa
+def dano_recebido(pokemon_atacante, pokemon_defensor, multiplicador_vantagem):
+    ataque = pokemon_atacante.atributos["ataque"] + pokemon_atacante.atributos["ataque_especial"]
+
+    defesa = pokemon_defensor.atributos["defesa"] + pokemon_defensor.atributos["defesa_especial"]
+    return (ataque / defesa) * multiplicador_vantagem * 100
+
+# calcula o valor total de uma "batalha" de pokemon pra pokemon
+def valor_total_batalha(pokemon_time, pokemon_ginasio):
+    vantagem_de_tipo_time_gerado = calcular_vantagem(pokemon_time, pokemon_ginasio)
+
+    vantagem_de_tipo_ginasio = calcular_vantagem(pokemon_ginasio, pokemon_time)
+
+    poder_time_gerado = poder_pokemon(pokemon_time) * vantagem_de_tipo_time_gerado
+
+    dano = dano_recebido(pokemon_ginasio, pokemon_time, vantagem_de_tipo_ginasio)
+
+    pontos_de_vida_restante = pokemon_time.atributos["pontos_de_vida"] - dano
+
+    if pontos_de_vida_restante <= 0:
+        return -1000
+    return poder_time_gerado + pontos_de_vida_restante
